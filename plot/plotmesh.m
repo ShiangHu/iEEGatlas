c03f68804f134c66532c4636a614b33f314e783c
shianghu <shiandyhu@gmail.com>
2020-01-05 17:23:47 +0800

    build atlas for brainstorm

--- plot/plotmesh.m
+++ plot/plotmesh.m
@@ -5,7 +5,7 @@ load WakefulnessMatlabFile;
 options.figname='Normal';
 patch.faces=[FacesLeft;FacesRight+size(NodesLeft,1)];
 patch.vertices=[NodesLeft;NodesRight];
-options.texture=rand(size(patch.vertices,1),1);
+options.texture=zeros(size(patch.vertices,1),1);
 spm_eeg_render(patch,options);
 
 % % Right HmS
@@ -16,11 +16,11 @@ spm_eeg_render(patch,options);
 % spm_eeg_render(patch,options);
 
 % Inflated
-options.figname='Inflated';
-patch.faces=[FacesLeft;FacesRight+size(NodesLeftInflated,1)];
-patch.vertices=[NodesLeftInflated;NodesRightInflated];
-options.texture=rand(size(patch.vertices,1),1);
-spm_eeg_render(patch,options);
+% options.figname='Inflated';
+% patch.faces=[FacesLeft;FacesRight+size(NodesLeftInflated,1)];
+% patch.vertices=[NodesLeftInflated;NodesRightInflated];
+% options.texture=rand(size(patch.vertices,1),1);
+% spm_eeg_render(patch,options);
 
 % % Right Inflated
 % options.figname='Right Inflated';
@@ -29,3 +29,32 @@ spm_eeg_render(patch,options);
 % options.texture=rand(size(NodesRightInflated,1),1);
 % spm_eeg_render(patch,options);
 
+
+% create atlas for brainsotrm visualization and simulation
+close all;
+cortex15002 = load('/home/shu/brainstorm_db/Protocol01/anat/Subject01/tess_cortex_pial_low.mat');
+Scouts = cortex15002.Atlas(3).Scouts;
+Region = [NodesRegionLeft;NodesRegionRight];
+Scouts(40:end) = [];
+for i=1:length(RegionName)
+    Scouts(i).Vertices = find(Region==i)';
+    Scouts(i).Label = RegionName{i};
+    Scouts(i).Region = [];
+    Scouts(i).Seed = [];
+end
+clearvars -except Scouts patch cortex15002;
+cortex_iEEG = cortex15002;
+cortex_iEEG.Vertices = patch.vertices;
+cortex_iEEG.Faces = patch.faces;
+cortex_iEEG.Atlas = struct('Name','default','Scouts',Scouts);
+cortex_iEEG.VertNormals = [];
+cortex_iEEG.VertConn = [];
+cortex_iEEG.Comment = 'cortex_iEEG';
+cortex_iEEG.Curvature = [];
+cortex_iEEG.SulciMap = [];
+cortex_iEEG.tess2mri_interp = [];
+cortex_iEEG.iAtlas = 1;
+cortex_iEEG.Reg.Sphere.Vertices=[];
+% cortex_iEEG=rmfield(cortex_iEEG,'mrimask');
+cortex_iEEG.Reg.Sphere.Vertices=[];
+% import cortex_iEEG from MATLAB workspace to Brainstorm subject
