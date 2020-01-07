6f0ce9e5af362c183a0bf79e4f17a5e1a289b97a
shianghu <shiandyhu@gmail.com>
2020-01-07 15:10:26 +0800

    test if vertices override the skull

--- plot/plotmesh.m
+++ plot/plotmesh.m
@@ -66,8 +66,8 @@ nver = size(tmp,1);
 tmp = tmp./repmat(max(tmp) - min(tmp),nver,1).*repmat(max(Ref)-min(Ref),nver,1);
 cortex_iEEG.Vertices = tmp + repmat(max(Ref) - max(tmp),nver,1);
 cortex_iEEG.Reg.Sphere.Vertices=[];
+% cortex_iEEG.Vertices = cortex_iEEG.Vertices * 0.8; % avoid the vertives override the innerskull
 % cortex_iEEG=rmfield(cortex_iEEG,'mrimask');
-% cortex_iEEG.Reg.Sphere.Vertices=[];
 % import cortex_iEEG from MATLAB workspace to Brainstorm subject
 
 iEEGmat = load('WakefulnessMatlabFile.mat');
